c02baf13b405e23869225b1f7c7a0befb06813a4
andrewbares <andrewbares@outlook.com>
2015-08-17 10:42:38 -0700

    Added some badge tests

--- Windows 10/NotificationsExtensions.Win10.Test.Portable/Test_Badge_Xml.cs
+++ Windows 10/NotificationsExtensions.Win10.Test.Portable/Test_Badge_Xml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NotificationsExtensions.Badges;
 
 namespace NotificationsExtensions.Win10.Test.Portable
 {
@@ -7,13 +8,61 @@ namespace NotificationsExtensions.Win10.Test.Portable
     public class Test_Badge_Xml
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Badge_Xml_Numeric_0()
         {
+            AssertBadgeValue("0", new BadgeNumericNotificationContent(0));
         }
 
-        //private static void AssertPayload(string expectedXml, I toast)
-        //{
-        //    AssertHelper.AssertXml(expectedXml, toast.GetContent());
-        //}
+        [TestMethod]
+        public void Test_Badge_Xml_Numeric_1()
+        {
+            AssertBadgeValue("1", new BadgeNumericNotificationContent(1));
+        }
+
+        [TestMethod]
+        public void Test_Badge_Xml_Numeric_2()
+        {
+            AssertBadgeValue("2", new BadgeNumericNotificationContent(2));
+        }
+
+        [TestMethod]
+        public void Test_Badge_Xml_Numeric_546()
+        {
+            AssertBadgeValue("546", new BadgeNumericNotificationContent(546));
+        }
+
+        [TestMethod]
+        public void Test_Badge_Xml_Numeric_Max()
+        {
+            AssertBadgeValue(uint.MaxValue.ToString(), new BadgeNumericNotificationContent(uint.MaxValue));
+        }
+
+        [TestMethod]
+        public void Test_Badge_Xml_Glyph_None()
+        {
+            AssertBadgeValue("none", new BadgeGlyphNotificationContent(GlyphValue.None));
+        }
+
+        [TestMethod]
+        public void Test_Badge_Xml_Glyph_Alert()
+        {
+            AssertBadgeValue("alert", new BadgeGlyphNotificationContent(GlyphValue.Alert));
+        }
+
+        [TestMethod]
+        public void Test_Badge_Xml_Glyph_Error()
+        {
+            AssertBadgeValue("error", new BadgeGlyphNotificationContent(GlyphValue.Error));
+        }
+
+        private static void AssertBadgeValue(string expectedValue, INotificationContent notificationContent)
+        {
+            AssertPayload("<badge version='1' value='" + expectedValue + "'/>", notificationContent);
+        }
+
+        private static void AssertPayload(string expectedXml, INotificationContent notificationContent)
+        {
+            AssertHelper.AssertXml(expectedXml, notificationContent.GetContent());
+        }
     }
 }
