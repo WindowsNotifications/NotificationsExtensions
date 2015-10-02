44e54922f7e5cfc250e55a6b0b0c3bd055194979
anbare <anbare@microsoft.com>
2015-10-01 17:14:07 -0700

    Updated script for generating NuGet package
    Added timestamp displaying so it shows the timestamps of DLL's, so you
    can ensure that the DLL's have properly been updated before generating
    the NuGet package

--- Windows 10/GenerateNugetPackage.bat
+++ Windows 10/GenerateNugetPackage.bat
@@ -1,5 +1,32 @@
 @echo off
 
+set files=NotificationsExtensions.Win10.Portable\bin\Release\NotificationsExtensions.Win10.Portable.dll
+set files=%files% NotificationsExtensions.Win10.Portable\bin\Release\NotificationsExtensions.Win10.Portable.pdb
+set files=%files% NotificationsExtensions.Win10.Portable\bin\Release\NotificationsExtensions.Win10.Portable.xml
+
+set files=%files% NotificationsExtensions.Win10.WinRT\bin\Release\NotificationsExtensions.winmd
+set files=%files% NotificationsExtensions.Win10.WinRT\bin\Release\NotificationsExtensions.pri
+set files=%files% NotificationsExtensions.Win10.WinRT\bin\Release\NotificationsExtensions.pdb
+set files=%files% NotificationsExtensions.Win10.WinRT\bin\Release\NotificationsExtensions.xml
+
+set files=%files% NotificationsExtensions.Win10.NETCore\bin\Release\NotificationsExtensions.Win10.NETCore.dll
+set files=%files% NotificationsExtensions.Win10.NETCore\bin\Release\NotificationsExtensions.Win10.NETCore.pdb
+set files=%files% NotificationsExtensions.Win10.NETCore\bin\Release\NotificationsExtensions.Win10.NETCore.xml
+
+FOR %%f IN (%files%) DO IF NOT EXIST %%f call :file_not_found %%f
+
+
+echo Here are the current timestamps on the DLL's...
+echo.
+
+FOR %%f IN (%files%) DO ECHO %%~tf %%f
+
+echo.
+
+PAUSE
+
+
+
 echo Welcome, let's create a new NuGet package for NotificationsExtensions.Win10!
 echo.
 
@@ -12,3 +39,13 @@ if not exist "..\NugetPackages" mkdir "..\NugetPackages"
 PAUSE
 
 explorer ..\NugetPackages
+
+
+
+
+exit
+:file_not_found
+
+echo File not found: %1
+PAUSE
+exit
