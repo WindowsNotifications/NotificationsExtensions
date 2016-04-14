75cdf33ca0c26b2bf45dfc50a078901085d5a048
anbare <anbare@microsoft.com>
2016-04-14 13:51:37 -0700

    Revert "Updated photos template to only allow 9 images"
    This reverts commit b87906e611d5e5955751b18b2746e699afae5246.

--- Windows 10/NotificationsExtensions.Test/Test_Tile_Xml.cs
+++ Windows 10/NotificationsExtensions.Test/Test_Tile_Xml.cs
@@ -2284,7 +2284,10 @@ namespace NotificationsExtensions.Win10.Test
                     new TileImageSource("6.jpg"),
                     new TileImageSource("7.jpg"),
                     new TileImageSource("8.jpg"),
-                    new TileImageSource("9.jpg")
+                    new TileImageSource("9.jpg"),
+                    new TileImageSource("10.jpg"),
+                    new TileImageSource("11.jpg"),
+                    new TileImageSource("12.jpg")
                 }
             };
 
@@ -2298,6 +2301,9 @@ namespace NotificationsExtensions.Win10.Test
                 <image src='7.jpg'/>
                 <image src='8.jpg'/>
                 <image src='9.jpg'/>
+                <image src='10.jpg'/>
+                <image src='11.jpg'/>
+                <image src='12.jpg'/>
             </binding>", new TileBinding()
             {
                 Content = content
@@ -2322,14 +2328,17 @@ namespace NotificationsExtensions.Win10.Test
                         new TileImageSource("7.jpg"),
                         new TileImageSource("8.jpg"),
                         new TileImageSource("9.jpg"),
-                        new TileImageSource("10.jpg")
+                        new TileImageSource("10.jpg"),
+                        new TileImageSource("11.jpg"),
+                        new TileImageSource("12.jpg"),
+                        new TileImageSource("13.jpg")
                     }
                 };
             }
 
             catch { return; }
 
-            Assert.Fail("Exception should have been thrown, adding more than 9 images isn't supported.");
+            Assert.Fail("Exception should have been thrown, adding more than 12 images isn't supported.");
         }
 
         #endregion
