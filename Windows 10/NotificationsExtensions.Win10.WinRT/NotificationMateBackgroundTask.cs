fc150c8c7a60a4aa38911a3ce20c7684490fe0ff
anbare <anbare@microsoft.com>
2016-04-29 14:36:39 -0700

    Added comments to NotificationMateBackgroundTask
    The class, and some properties were missing comments. Also added an
    explanation that the Register method can be called multiple times, and
    explained that you can simply place the method call in your app
    initialization.

--- Windows 10/NotificationsExtensions.Win10.WinRT/NotificationMateBackgroundTask.cs
+++ Windows 10/NotificationsExtensions.Win10.WinRT/NotificationMateBackgroundTask.cs
@@ -4,13 +4,21 @@ using Windows.ApplicationModel.Background;
 
 namespace NotificationsExtensions
 {
+    /// <summary>
+    /// This class is an already-implemented background task for handling the <see cref="ToastNotificationHistoryChangedTrigger"/> to update your primary Live Tile and badge count. All you have to do is call the <see cref="NotificationMateBackgroundTask.RegisterBackgroundTask"/> method to register the background task.
+    /// </summary>
     public sealed class NotificationMateBackgroundTask : IBackgroundTask
     {
+        /// <summary>
+        /// Gets a string representing the name used to register the background task. If you want to unregister the background task, you can find the background task with this given name and unregister it.
+        /// </summary>
         public static string TaskName { get; } = "NotificationMateBackgroundTask";
 
         /// <summary>
         /// Register a background task to automatically keep your tile in sync when your Toast notifications are added or removed.
         /// NOTE: You must register the background task in your manifest for it to fire. See the remarks section for more details.
+        /// The easiest way to implement NotificationMate is to call this method upon your app's initilization. It is safe to call this
+        /// method multiple times - the method checks if the background task isn't registered yet before registering a new task.
         /// </summary>
         /// <remarks>
         /// You must include the following entry in your manifest for background task registration to work.
@@ -44,6 +52,11 @@ namespace NotificationsExtensions
             }
         }
 
+        /// <summary>
+        /// This method is executed when the background task is triggered as a result of a change in the app's notifications
+        /// that wasn't caused by the app's local code sending a notification itself.
+        /// </summary>
+        /// <param name="taskInstance"></param>
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             NotificationMate.SyncTileWithActionCenter();
