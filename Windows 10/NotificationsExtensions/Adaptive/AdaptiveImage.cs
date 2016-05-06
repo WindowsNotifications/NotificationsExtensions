eed8753d450e83e936db15ce2f655fcbfb29fd50
Andrew Bares <andrewbares@outlook.com>
2016-05-06 09:00:55 -0700

    Some progress on adding adaptive toast (committing so I can switch code to another machine since VS is going wonky on this one)

---
+++ Windows 10/NotificationsExtensions/Adaptive/AdaptiveImage.cs
@@ -0,0 +1,64 @@
+﻿using NotificationsExtensions.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationsExtensions.Adaptive
+{
+    public sealed class AdaptiveImage : ITileSubgroupChild, ITileAdaptiveChild
+    {
+        /// <summary>
+        /// Initializes a new inline image.
+        /// </summary>
+        public AdaptiveImage() { }
+
+        /// <summary>
+        /// Provide the source of the image and other image source related properties.
+        /// </summary>
+        public TileImageSource Source { get; set; }
+
+        /// <summary>
+        /// Control the desired cropping of the image.
+        /// </summary>
+        public TileImageCrop Crop { get; set; } = Element_TileImage.DEFAULT_CROP;
+
+        /// <summary>
+        /// By default, images have an 8px margin around them. You can remove this margin by setting this property to true.
+        /// </summary>
+        public bool RemoveMargin { get; set; } = Element_TileImage.DEFAULT_REMOVE_MARGIN;
+
+        /// <summary>
+        /// The horizontal alignment of the image.
+        /// </summary>
+        public TileImageAlign Align { get; set; } = Element_TileImage.DEFAULT_ALIGN;
+
+        internal Element_TileImage ConvertToElement()
+        {
+            Element_TileImage image = new Element_TileImage()
+            {
+                Crop = Crop,
+                RemoveMargin = RemoveMargin,
+                Align = Align,
+                Placement = TileImagePlacement.Inline
+            };
+
+            if (Source == null)
+                throw new NullReferenceException("Source property is required on TileImage");
+
+            Source.PopulateElement(image);
+
+            return image;
+        }
+
+        /// <summary>
+        /// Returns the image's source string.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (Source == null)
+                return "Source is null";
+
+            return Source.ToString();
+        }
+    }
--- Windows 10/NotificationsExtensions/Tiles/Adaptive/TileImage.cs
+++ Windows 10/NotificationsExtensions/Tiles/Adaptive/TileImage.cs
@@ -6,7 +6,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved
 
 
-using System;
 
 namespace NotificationsExtensions.Tiles
 {
